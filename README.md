14a67d1015d07a1c1b8a57460941f156c079ccfe
Jan Sabbe <jan.sabbe@gmail.com>
2016-10-12 22:28:50 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # elm_petcupid
 Building PetCupid with Elm
+
+## Getting started
+
+* Run `npm install` then `npm start`. Go to http://localhost:8000
+* For easier development `npm install elm -g` 
--- index.elm
+++ index.elm
@@ -7,37 +7,3 @@ import Html.Events exposing (..)
 
 main =
     h1 [] [ text "Hello World!" ]
-
-
-type alias Model =
-    { counter : Int }
-
-
-model : Model
-model =
-    { counter = 0
-    }
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ div [] [ text (Basics.toString model.counter) ]
-        , div [] [ button [ onClick Increment ] [ text "+" ] ]
-        , div [] [ button [ onClick Decrement ] [ text "-" ] ]
-        ]
-
-
-type Msg
-    = Increment
-    | Decrement
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Increment ->
-            { model | counter = model.counter + 1 }
-
-        Decrement ->
-            { model | counter = model.counter - 1 }
