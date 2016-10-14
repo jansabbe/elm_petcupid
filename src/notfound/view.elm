69db9c98e8e65c066c8ebbe1588985f1ceca785a
okker <okke.reumers@cegeka.com>
2016-10-14 15:47:51 +0200

    Okke: page not found styling

--- src/notfound/view.elm
+++ src/notfound/view.elm
@@ -2,9 +2,15 @@ module NotFound.View exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Index.View exposing (petcupidHeader, petcupidFooter)
 
 notFoundView : Html msg
 notFoundView =
-    div [ style [("background-color", "powderblue")] ]
-        [ text "Not found"
-        ]
+    div [ ]
+        [petcupidHeader,
+        div [ style [("color", "white"), ("margin-top", "20px"), ("margin-bottom", "20px"),
+                ("font-size", "40px"), ("text-align", "center"), ("font-weight", "bold")]][text "Page not found"],
+        div [ style [("color", "white"), ("margin-top", "20px"), ("margin-bottom", "20px"),
+        ("font-size", "40px"), ("text-align", "center")]][text "Sorry, you will not find any love here"],
+        div [style [("text-align", "center"), ("padding-bottom", "50px")]] [ img [ src "assets/page-not-found.jpg" ] [] ],
+        petcupidFooter]
