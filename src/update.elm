c48032b798a5907c87ec6190b5a6d858a19acda6
Niki <niki.vandesbosch@gmail.com>
2016-10-14 12:48:53 +0200

    NikiV, Okker: divided code over multiple files

---
+++ src/update.elm
@@ -0,0 +1,24 @@
+module Update exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.App as App
+import Html.Events exposing (..)
+import String exposing (..)
+import Json.Decode
+
+import Models exposing (..)
+
+update : Msg -> Model -> Model
+update msg ({ search } as model) =
+    case msg of
+        TextSearch newSearchText ->
+            { model | search = { search | textSearch = newSearchText } }
+
+        Filter newFilter ->
+            { model | search = { search | kindFilter = newFilter } }
+
+        Select pet ->
+            { model | selectedPet = Just pet }
+
+
